a68191e67bb87ba1dbc6b73a0b244b93ca40acd8
Matt Steele <matthew_steele@live.com.au>
2022-01-07 15:42:57 +1100

    First commit for this repo

---
+++ cisco-devnet-dna.py
@@ -0,0 +1,15 @@
+import requests
+
+def get_auth_token(controller_ip=DNAC, username=DNAC_USER, password=DNAC_PASSWORD):
+    """ Authenticates with controller and returns a token to be used in subsequent API invocations
+    """
+
+    login_url = "https://{0}:{1}/dna/system/api/v1/auth/token".format(controller_ip, DNAC_PORT)
+    result = requests.post(url=login_url, auth=HTTPBasicAuth(DNAC_USER, DNAC_PASSWORD), verify=False)
+    result.raise_for_status()
+
+    token = result.json()["Token"]
+    return {
+        "controller_ip": controller_ip,
+        "token": token
+    }
