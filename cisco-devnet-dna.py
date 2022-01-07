2fffefecdf079591125f8cae4050f05400b5212f
Matt Steele <matthew_steele@live.com.au>
2022-01-07 16:15:00 +1100

    Finished cisco-devnet-dna.py

--- cisco-devnet-dna.py
+++ cisco-devnet-dna.py
@@ -1,4 +1,14 @@
+import sys
+import json
 import requests
+import os
+from requests.auth import HTTPBasicAuth
+requests.packages.urllib3.disable_warnings()
+
+DNAC=os.environ.get('DNAC','sandboxdnac.cisco.com')
+DNAC_PORT=os.environ.get('DNAC_PORT',443)
+DNAC_USER=os.environ.get('DNAC_USER','devnetuser')
+DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','Cisco123!')
 
 def get_auth_token(controller_ip=DNAC, username=DNAC_USER, password=DNAC_PASSWORD):
     """ Authenticates with controller and returns a token to be used in subsequent API invocations
@@ -13,3 +23,26 @@ def get_auth_token(controller_ip=DNAC, username=DNAC_USER, password=DNAC_PASSWOR
         "controller_ip": controller_ip,
         "token": token
     }
+
+def create_url(path, controller_ip=DNAC):
+    """ Helper function to create a DNAC API endpoint URL
+    """
+
+    return "https://%s:%s/api/v1/%s" % (controller_ip, DNAC_PORT, path)
+
+def get_url(url):
+    
+    url = create_url(path=url)
+    print(url)
+    token = get_auth_token()
+    headers = {'X-auth-token' : token['token']}
+    try:
+        response = requests.get(url, headers=headers, verify=False)
+    except requests.exceptions.RequestException as cerror:
+        print("Error processing request", cerror)
+        sys.exit(1)
+
+    return response.json()
+
+def list_network_devices():
+    return get_url("network-device")
